4d23af0e7392f2a15d3ba7cb841e472fa20ec863
sean.hollen <hollen.s@husky.neu.edu>
2021-12-28 13:56:26 -0500

    cut runtime by removing logs

--- stats-calculations/index.js
+++ stats-calculations/index.js
@@ -7,39 +7,39 @@ const executer = new MainExecuter()
 const titleBasics = Files.readTsvDataBuffer('title.basics')
 executer.readTitleBasics(titleBasics)
 console.log("\n*** read title basics ***")
-executer.logTitles(settings.logNumber)
+// executer.logTitles(settings.logNumber)
 
 const ratingsBasics = Files.readTsvDataFs('title.ratings')
 executer.readRatingsBasics(ratingsBasics, settings.minReviews);
 console.log("\n*** read rating basics ***")
-executer.logTitles(settings.logNumber)
+// executer.logTitles(settings.logNumber)
 
 executer.createSortedTitles()
 console.log("\n*** created sorted titles ***")
-executer.logTitles(settings.logNumber)
+// executer.logTitles(settings.logNumber)
 
 executer.assignOrderRatings()
 console.log("\n*** assigned order rating ***")
-executer.logTitles(settings.logNumber)
+// executer.logTitles(settings.logNumber)
 
 const crewBasics = Files.readTsvDataFs('title.crew')
 executer.readCrewBasics(crewBasics)
 console.log("\n*** read crew basics ***")
-executer.logDirectors(settings.logNumber)
+// executer.logDirectors(settings.logNumber)
 
 executer.calculateScores(settings.exponent)
 console.log("\n*** calculated scores ***")
-executer.logDirectors(settings.logNumber)
+// executer.logDirectors(settings.logNumber)
 
 executer.createSortedDirectors()
 console.log("\n*** created sorted directors list ***")
-executer.logSortedDirectors(settings.logNumber)
+e// executer.logSortedDirectors(settings.logNumber)
 
 const namesBasics = Files.readTsvDataBuffer('name.basics')
 executer.readNameBasics(namesBasics)
 // executer.logDirectors(settings.logNumber)
 
 const output = executer.generateOutput()
-console.log(output.slice(0, 1000), settings.toOutput)
+// console.log(output.slice(0, 1000), settings.toOutput)
 Files.writeToFile(output)
 
